28f6640aeb13b2e9d37015ec88e12f311ea1bbae
Callum Forrester <29771545+callumforrester@users.noreply.github.com>
2022-04-05 11:18:53 +0100

    Init worker in seprarte starup function (#8)

--- src/blueapi/utils/__init__.py
+++ src/blueapi/utils/__init__.py
@@ -1,3 +1,4 @@
+from .aio_threading import concurrent_future_to_aio_future
 from .thread_exception import handle_all_exceptions
 
-__all__ = ["handle_all_exceptions"]
+__all__ = ["handle_all_exceptions", "concurrent_future_to_aio_future"]

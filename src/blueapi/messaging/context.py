f47d25681be81bbfc7d4466f04f9a4dc03684443
Callum Forrester <callum.forrester@diamond.ac.uk>
2022-04-12 14:18:40 +0100

    Add helper to determine if we can reply

--- src/blueapi/messaging/context.py
+++ src/blueapi/messaging/context.py
@@ -6,3 +6,6 @@ from typing import Optional
 class MessageContext:
     destination: str
     reply_destination: Optional[str]
+
+    def can_reply(self) -> bool:
+        return self.reply_destination is not None

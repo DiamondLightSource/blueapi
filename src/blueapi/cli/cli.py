8c8b1f362312b73671213317b037a36656c23517
Callum Forrester <29771545+callumforrester@users.noreply.github.com>
2022-08-22 07:19:36 +0100

    Move service and controller into main source (#23)
    * Move examples into source directory
    
    * Make unified CLI

--- src/blueapi/cli/cli.py
+++ src/blueapi/cli/cli.py
@@ -13,6 +13,22 @@ from .amq import AmqClient
 
 
 @click.group(invoke_without_command=True)
+@click.version_option(version=__version__)
+@click.pass_context
+def main(ctx) -> None:
+    # if no command is supplied, run with the options passed
+    if ctx.invoked_subcommand is None:
+        print("Please invoke subcommand!")
+
+
+@main.command(name="worker")
+def start_worker():
+    from blueapi.service import main
+
+    main()
+
+
+@main.group()
 @click.option(
     "-h",
     "--host",
@@ -27,9 +43,8 @@ from .amq import AmqClient
     help="Broker port",
     default=61613,
 )
-@click.version_option(version=__version__)
 @click.pass_context
-def main(ctx, host: str, port: int) -> None:
+def controller(ctx, host: str, port: int):
     # if no command is supplied, run with the options passed
     if ctx.invoked_subcommand is None:
         print("Please invoke subcommand!")
@@ -39,7 +54,7 @@ def main(ctx, host: str, port: int) -> None:
     client.app.connect()
 
 
-@main.command(name="plans")
+@controller.command(name="plans")
 @click.pass_context
 def get_plans(ctx) -> None:
     client: AmqClient = ctx.obj["client"]
@@ -49,14 +64,14 @@ def get_plans(ctx) -> None:
         print("\t" + plan["name"])  # type: ignore
 
 
-@main.command(name="abilities")
+@controller.command(name="abilities")
 @click.pass_context
 def get_abilities(ctx) -> None:
     client: AmqClient = ctx.obj["client"]
     print(client.get_abilities())
 
 
-@main.command(name="run")
+@controller.command(name="run")
 @click.argument("name", type=str)
 @click.option("-p", "--parameters", type=str, help="Parameters as valid JSON")
 @click.pass_context

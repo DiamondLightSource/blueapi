043421f758fc6bc92a4f696f0d63bc071d0becf4
Callum Forrester <callum.forrester@diamond.ac.uk>
2022-10-03 14:52:41 +0100

    Tests for find device

--- tests/core/test_context.py
+++ tests/core/test_context.py
@@ -1,10 +1,15 @@
-from typing import Dict
+from typing import Dict, List, Union
 
 import pytest
 from bluesky.protocols import Descriptor, Reading, SyncOrAsync
-from ophyd.sim import SynAxis
+from ophyd.sim import SynAxis, SynGauss
 
-from blueapi.core import BlueskyContext, MsgGenerator, PlanGenerator
+from blueapi.core import (
+    BlueskyContext,
+    MsgGenerator,
+    PlanGenerator,
+    is_bluesky_compatible_device,
+)
 
 #
 # Dummy plans
@@ -40,11 +45,37 @@ def has_typeless_params(foo, bar) -> MsgGenerator:
 #
 
 SIM_MOTOR_NAME = "sim"
+SIM_DET_NAME = "sim_det"
 
 
 @pytest.fixture
 def sim_motor() -> SynAxis:
     return SynAxis(name=SIM_MOTOR_NAME)
+
+
+@pytest.fixture
+def sim_detector(sim_motor: SynAxis) -> SynAxis:
+    return SynGauss(
+        name=SIM_DET_NAME,
+        motor=sim_motor,
+        motor_field=SIM_MOTOR_NAME,
+        center=0.0,
+        Imax=1,
+        labels={"detectors"},
+    )
+
+
+@pytest.fixture
+def empty_context() -> BlueskyContext:
+    return BlueskyContext()
+
+
+@pytest.fixture
+def devicey_context(sim_motor: SynAxis, sim_detector: SynGauss) -> BlueskyContext:
+    ctx = BlueskyContext()
+    ctx.device(sim_motor)
+    ctx.device(sim_detector)
+    return ctx
 
 
 class SomeConfigurable:
@@ -61,42 +92,51 @@ def some_configurable() -> SomeConfigurable:
 
 
 @pytest.mark.parametrize("plan", [has_no_params, has_one_param, has_some_params])
-def test_add_plan(plan: PlanGenerator) -> None:
-    ctx = BlueskyContext()
-    ctx.plan(plan)
-    assert plan.__name__ in ctx.plans
+def test_add_plan(empty_context: BlueskyContext, plan: PlanGenerator) -> None:
+    empty_context.plan(plan)
+    assert plan.__name__ in empty_context.plans
 
 
 @pytest.mark.parametrize(
     "plan", [has_typeless_param, has_typed_and_typeless_params, has_typeless_params]
 )
-def test_add_invalid_plan(plan: PlanGenerator) -> None:
-    ctx = BlueskyContext()
+def test_add_invalid_plan(empty_context: BlueskyContext, plan: PlanGenerator) -> None:
     with pytest.raises(TypeError):
-        ctx.plan(plan)
+        empty_context.plan(plan)
 
 
-def test_add_named_device(sim_motor: SynAxis) -> None:
-    ctx = BlueskyContext()
-    ctx.device(sim_motor)
-    assert ctx.devices[SIM_MOTOR_NAME] is sim_motor
+def test_add_named_device(empty_context: BlueskyContext, sim_motor: SynAxis) -> None:
+    empty_context.device(sim_motor)
+    assert empty_context.devices[SIM_MOTOR_NAME] is sim_motor
 
 
-def test_add_nameless_device(some_configurable: SomeConfigurable) -> None:
-    ctx = BlueskyContext()
-    ctx.device(some_configurable, "conf")
-    assert ctx.devices["conf"] is some_configurable
+def test_add_nameless_device(
+    empty_context: BlueskyContext, some_configurable: SomeConfigurable
+) -> None:
+    empty_context.device(some_configurable, "conf")
+    assert empty_context.devices["conf"] is some_configurable
 
 
 def test_add_nameless_device_without_override(
+    empty_context: BlueskyContext,
     some_configurable: SomeConfigurable,
 ) -> None:
-    ctx = BlueskyContext()
     with pytest.raises(KeyError):
-        ctx.device(some_configurable)
+        empty_context.device(some_configurable)
 
 
-def test_override_device_name(sim_motor: SynAxis) -> None:
-    ctx = BlueskyContext()
-    ctx.device(sim_motor, "foo")
-    assert ctx.devices["foo"] is sim_motor
+def test_override_device_name(
+    empty_context: BlueskyContext, sim_motor: SynAxis
+) -> None:
+    empty_context.device(sim_motor, "foo")
+    assert empty_context.devices["foo"] is sim_motor
+
+
+@pytest.mark.parametrize(
+    "addr", ["sim", "sim_det", "sim.setpoint", ["sim"], ["sim", "setpoint"]]
+)
+def test_lookup_device(
+    devicey_context: BlueskyContext, addr: Union[str, List[str]]
+) -> None:
+    device = devicey_context.find_device(addr)
+    assert is_bluesky_compatible_device(device)

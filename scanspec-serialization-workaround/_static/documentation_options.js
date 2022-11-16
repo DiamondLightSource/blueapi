943c7ca78be2ee178a6a3f63040349b4d0d4384b
callumforrester <callumforrester@users.noreply.github.com>
2022-11-16 16:21:22 +0000

    deploy: 34150646f83615ced16ab78a5bf49e7d4691adc9

--- scanspec-serialization-workaround/_static/documentation_options.js
+++ scanspec-serialization-workaround/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev64+g0ab21a2',
+    VERSION: '0.1.dev65+g3415064',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

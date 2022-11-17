09e116d962568497d5a8792ec0e6a365f4c84c60
callumforrester <callumforrester@users.noreply.github.com>
2022-11-17 13:40:57 +0000

    deploy: 9cfb61f55f688f7458de00f6c243aaad420b4e07

--- reduce-coverage-requirements/_static/documentation_options.js
+++ reduce-coverage-requirements/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev64+g6ab3bd9',
+    VERSION: '0.1.dev65+g9cfb61f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

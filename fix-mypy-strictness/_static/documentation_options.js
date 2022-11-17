c2fc9a24c3ee23cd9ed7aa658dde84e5ee6402ab
callumforrester <callumforrester@users.noreply.github.com>
2022-11-17 13:28:25 +0000

    deploy: fd95dd80d012dd523bfd41fc8aec8c88c9458aca

--- fix-mypy-strictness/_static/documentation_options.js
+++ fix-mypy-strictness/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev64+g7e808d3',
+    VERSION: '0.1.dev65+gfd95dd8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

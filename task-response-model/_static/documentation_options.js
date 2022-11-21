b574e544e54ebf3bffd150712288bff2ea78195a
callumforrester <callumforrester@users.noreply.github.com>
2022-11-21 17:30:16 +0000

    deploy: c816f8aea82962e05deb9dbafb015129c6241963

--- task-response-model/_static/documentation_options.js
+++ task-response-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev69+gd25e33d',
+    VERSION: '0.1.dev71+gc816f8a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

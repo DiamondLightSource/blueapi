ee5cd4221cea6a36a9ca2b40fecdaa1884b6d003
callumforrester <callumforrester@users.noreply.github.com>
2023-01-12 10:06:01 +0000

    deploy: 911033e4cb14b9f53fa2f552f70d4c0e4542bb06

--- helm/_static/documentation_options.js
+++ helm/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev81+g9f2c1ba',
+    VERSION: '0.1.dev82+g911033e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

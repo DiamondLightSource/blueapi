1c6db0f3c184c5a8ce3deaf52d825eaabe9987ab
callumforrester <callumforrester@users.noreply.github.com>
2023-03-08 09:34:30 +0000

    deploy: 83f266edd13b75ce06d721733dd37cb44c9b2543

--- stomp-reconnect/_static/documentation_options.js
+++ stomp-reconnect/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev10+g0bd839a',
+    VERSION: '0.1.1.dev11+g83f266e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

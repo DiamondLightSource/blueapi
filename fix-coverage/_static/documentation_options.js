1de3517da6e04a725dc10d43798a4cfc47434611
callumforrester <callumforrester@users.noreply.github.com>
2023-03-08 12:02:38 +0000

    deploy: 16cc15694d65fdfa8ea96734bec1b0f7723b750e

--- fix-coverage/_static/documentation_options.js
+++ fix-coverage/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev2+g64ce000',
+    VERSION: '0.1.1.dev9+g16cc156',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

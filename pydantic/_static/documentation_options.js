37ad624d0e38acea2a535cdd428a6cf5c957b19a
callumforrester <callumforrester@users.noreply.github.com>
2023-03-16 08:33:57 +0000

    deploy: 74257208979fd3692100672ac8df3548f11c866c

--- pydantic/_static/documentation_options.js
+++ pydantic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.5.dev3+g39ad35d',
+    VERSION: '0.1.1.dev6+g7425720',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

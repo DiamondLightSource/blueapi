a4049af6f21e19a59f592fbad09a388c3a5251d6
callumforrester <callumforrester@users.noreply.github.com>
2023-04-21 13:58:46 +0000

    deploy: 4c35abb3bd498b201079f05c0e3386556083830c

--- camel-case/_static/documentation_options.js
+++ camel-case/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.1.dev3+ge22bb3f',
+    VERSION: '0.2.1.dev4+g4c35abb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

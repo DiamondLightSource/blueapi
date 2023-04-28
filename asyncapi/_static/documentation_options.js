888e46ac1a5dc57be40983ef7d7db34326a1a87a
keithralphs <keithralphs@users.noreply.github.com>
2023-04-28 08:36:09 +0000

    deploy: 66b827b7d82f14e9389832c5570bd777d41c45fa

--- asyncapi/_static/documentation_options.js
+++ asyncapi/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3b2.dev8+gdca6413',
+    VERSION: '0.2.3b2.dev9+g66b827b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

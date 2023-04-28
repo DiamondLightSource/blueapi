5f152c4de442c483ac0bcacf452180fe29c6f252
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-04-28 10:00:33 +0000

    deploy: d0a8b51662cead3a50ac7bb00923ac1b94f66bd1

--- async-worker/_static/documentation_options.js
+++ async-worker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3b2.dev5+gdc1fb89',
+    VERSION: '0.2.3b2.dev6+gd0a8b51',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

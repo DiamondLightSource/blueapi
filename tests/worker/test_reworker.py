d023c36c6dacf8ec4e4506563be2f0d27a4545d2
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-05-02 09:34:36 +0100

    Refactor worker tests to extract events in common function

--- tests/worker/test_reworker.py
+++ tests/worker/test_reworker.py
@@ -1,8 +1,11 @@
+import itertools
 from concurrent.futures import Future
+from threading import Event
+from typing import Callable, List, TypeVar
 
 import pytest
 
-from blueapi.core import BlueskyContext
+from blueapi.core import BlueskyContext, EventStream
 from blueapi.worker import (
     RunEngineWorker,
     RunPlan,
@@ -48,20 +51,19 @@ def test_multi_start(worker: Worker) -> None:
 
 def test_submit(worker: Worker) -> None:
     worker.start()
-    started: Future[WorkerEvent] = Future()
 
-    def process_event(event: WorkerEvent, task_id: str) -> None:
-        started.set_result(event)
-
-    worker.worker_events.subscribe(process_event)
+    events: "Future[List[WorkerEvent]]" = take_n_events(
+        worker.worker_events,
+        1,
+    )
     worker.submit_task(
         "test",
         RunPlan(
             name="sleep",
-            params={"time": 0.1},
+            params={"time": 0.0},
         ),
     )
-    result = started.result(timeout=5.0)
+    result = events.result(timeout=5.0)[0]
     assert not result.errors
     assert result.state is WorkerState.RUNNING
 
@@ -102,3 +104,32 @@ def test_submit_multiple_tasks_at_once(
     result = started.result(timeout=plan["params"]["time"])
     assert not result.errors
     assert result.state is WorkerState.RUNNING
+
+
+E = TypeVar("E")
+S = TypeVar("S")
+
+
+def take_n_events(
+    stream: EventStream[E, S],
+    num: int,
+) -> "Future[List[E]]":
+    count = itertools.count()
+    return take_events(stream, lambda _: next(count) >= num)
+
+
+def take_events(
+    stream: EventStream[E, S],
+    cutoff_predicate: Callable[[E], bool],
+) -> "Future[List[E]]":
+    events: List[E] = []
+    future: "Future[List[E]]" = Future()
+
+    def on_event(event: E, event_id: str) -> None:
+        events.append(event)
+        if cutoff_predicate(event):
+            future.set_result(events)
+
+    sub = stream.subscribe(on_event)
+    future.add_done_callback(lambda _: stream.unsubscribe(sub))
+    return future

bba4e030121646be92161cd42bdeb2c663086004
callumforrester <callumforrester@users.noreply.github.com>
2023-05-03 08:13:25 +0000

    deploy: ee6803027ed22e2bd4f0f7796dd98bd8887a33d0

--- rename-cli/_static/documentation_options.js
+++ rename-cli/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3b2.dev6+g6f7ca1d',
+    VERSION: '0.2.3b2.dev7+gee68030',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

2ad5596e339e14ade34753752f69b7c51d66acef
keithralphs <keithralphs@users.noreply.github.com>
2023-05-05 12:50:33 +0000

    deploy: db1f3b74bad8b9143567f0ab2ae8a6333ef47b4d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev1+g3cc3d19',
+    VERSION: '0.2.4.dev2+gdb1f3b7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

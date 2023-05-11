e184f2b60513636be3d3d7d634fb282c34b656f6
abbiemery <abbiemery@users.noreply.github.com>
2023-05-11 16:11:40 +0000

    deploy: adac983138aa3255502ab0dd7386f31748c3982c

--- 121_dodal_integration/_static/documentation_options.js
+++ 121_dodal_integration/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev27+g946e188',
+    VERSION: '0.2.4.dev24+gadac983',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

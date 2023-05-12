17b207fe759ed299ba620755b625e592313d06f9
tpoliaw <tpoliaw@users.noreply.github.com>
2023-05-12 13:18:39 +0000

    deploy: 56c3c469f31bf77c28b063ef96822ba7525b367f

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev10+g368e981',
+    VERSION: '0.2.4.dev11+g56c3c46',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

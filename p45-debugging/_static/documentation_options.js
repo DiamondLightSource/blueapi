ef334f2c06b088538a6c576b5c9e1dd3de80608c
joeshannon <joeshannon@users.noreply.github.com>
2023-05-15 10:39:17 +0000

    deploy: c87d1a3448fcbf0a7443dd7cd6cca1a5344324a6

---
+++ p45-debugging/_static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.2.3b2.dev5+gc87d1a3',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};

f80b733405ac07c174278b03659be42955a1d3e4
DiamondJoseph <53935796+DiamondJoseph@users.noreply.github.com>
2023-05-17 16:14:17 +0100

    Update device_lookup.py

--- src/blueapi/core/device_lookup.py
+++ src/blueapi/core/device_lookup.py
@@ -50,5 +50,19 @@ def find_component(obj: Any, addr: List[str]) -> Optional[D]:
         )
 
 
-def inject(x):
-    return x
+def inject(name: str):
+    """
+    Function to mark a default argument of a plan method as a reference to a device
+    that is stored in the Blueapi context.
+    Bypasses mypy linting, returning x as Any and therefore valid as a default
+    argument.
+    
+    Args:
+        name (str): Name of a device to be fetched from the Blueapi context
+    
+    Returns:
+        Any: name but without typing checking, valid as any default type
+       
+    """
+    
+    return name

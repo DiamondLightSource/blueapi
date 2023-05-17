6b9039635bc17e6c45575af2b2bf10fe0d3bb680
Joe Shannon <joe.shannon@diamond.ac.uk>
2023-05-17 12:05:54 +0100

    Add module for test devices

---
+++ tests/worker/devices.py
@@ -0,0 +1,48 @@
+# Devices to use for worker tests
+
+from bluesky.protocols import Movable, Status
+from ophyd import Device, DeviceStatus
+
+
+class AdditionalUpdateStatus(DeviceStatus):
+    """
+    Status which emits an additional update to watchers
+    after the status is finished.
+    """
+
+    def add_callback(self, callback):
+        retval = super().add_callback(callback)
+        return retval
+
+    def watch(self, func):
+        """
+        set_finished called here instead of add_callback
+        to prevent race conditions
+        """
+        self.set_finished()
+        return super().watch(func)
+
+    def _run_callbacks(self) -> None:
+        super()._run_callbacks()
+        for watcher in self._watchers:
+            watcher(
+                name="STATUS_AFTER_FINISH",
+                current=0.0,
+                initial=1.0,
+                target=2.0,
+                unit="kg",
+                precision=0,
+                fraction=0.5,
+                time_elapsed=1.0,
+                time_remaining=1.0,
+            )
+
+
+class AdditionalStatusDevice(Device, Movable):
+    def set(self, value: float) -> Status:
+        status = AdditionalUpdateStatus(self)
+        return status
+
+
+def additional_status_device(name="additional_status_device") -> AdditionalStatusDevice:
+    return AdditionalStatusDevice(name=name)

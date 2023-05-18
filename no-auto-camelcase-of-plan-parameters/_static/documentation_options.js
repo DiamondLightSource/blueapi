d1b6c63243ca01b9fcc42e6db3fd33c8abd4fa35
callumforrester <callumforrester@users.noreply.github.com>
2023-05-18 08:38:22 +0000

    deploy: 648e2123a787d607eb9b30cacfe3d44f2316069b

--- no-auto-camelcase-of-plan-parameters/_static/documentation_options.js
+++ no-auto-camelcase-of-plan-parameters/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3b2.dev11+g412e19d',
+    VERSION: '0.2.4.dev24+g648e212',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

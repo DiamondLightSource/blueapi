a9454e74ac28745161c4264351aff88833050626
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-05-23 10:47:43 +0000

    deploy: a1988dce267b109f088ed5a32a321026957663b5

--- status/_static/documentation_options.js
+++ status/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev35+g52b7355f',
+    VERSION: '0.2.4.dev34+ga1988dce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

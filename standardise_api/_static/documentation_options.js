79bb45bd34945f275f298c879ddf9930c8f6596b
keithralphs <keithralphs@users.noreply.github.com>
2023-05-23 12:11:56 +0000

    deploy: 7f17e327b1ffb6653fe3b1512e5bd1953cf789c9

--- standardise_api/_static/documentation_options.js
+++ standardise_api/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev31+gb57e91aa',
+    VERSION: '0.2.4.dev32+g7f17e327',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

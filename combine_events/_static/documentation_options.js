626d0e3343d8b7fc32bc013fc194ca50b7b1088b
tpoliaw <tpoliaw@users.noreply.github.com>
2023-05-24 11:28:53 +0000

    deploy: cebd963cd4e79be7a8e960f4b767bcff03d9d16c

--- combine_events/_static/documentation_options.js
+++ combine_events/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev33+gbf256e90',
+    VERSION: '0.2.4.dev38+gcebd963c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

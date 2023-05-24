ee2db2d4fb4ef9f736ec885c3b2cc18fc70ca8f3
callumforrester <callumforrester@users.noreply.github.com>
2023-05-24 12:32:41 +0000

    deploy: 736cd4a859bc3cae85eb013c9965c8d52b721e0f

--- transaction-api/_static/documentation_options.js
+++ transaction-api/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev35+ge5c00f4b',
+    VERSION: '0.2.4.dev38+g736cd4a8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

f3569201c6993309dd4c0f6df8b8312e05d4136b
joeshannon <joeshannon@users.noreply.github.com>
2023-05-25 11:13:57 +0000

    deploy: d4f8c39a593b42d670179bcc5476b29d4ff4cb04

--- update-openapi/_static/documentation_options.js
+++ update-openapi/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev36+g186c4df2',
+    VERSION: '0.2.4.dev37+gd4f8c39a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

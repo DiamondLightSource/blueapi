fe36f18784a6d6cd5a860d3191fe2d9a27ea03de
callumforrester <callumforrester@users.noreply.github.com>
2023-05-25 13:26:24 +0000

    deploy: d2fe4cf52b7ed1ce9dbb900e2ddb3188a7978a97

--- openapi-checks/_static/documentation_options.js
+++ openapi-checks/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev42+g0bd0573a',
+    VERSION: '0.2.4.dev43+gd2fe4cf5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

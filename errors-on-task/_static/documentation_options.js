bbf8d6c3659d74da447587e7f3a8fd341b08e968
callumforrester <callumforrester@users.noreply.github.com>
2023-05-25 10:27:57 +0000

    deploy: 194a78dd1727abccd8675af7997a5b9df2318e1c

--- errors-on-task/_static/documentation_options.js
+++ errors-on-task/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev37+g1fd4b8ed',
+    VERSION: '0.2.4.dev38+g194a78dd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

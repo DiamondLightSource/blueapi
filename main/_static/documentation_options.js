b457991ce11cfa501c096576c6300ea2e6525701
callumforrester <callumforrester@users.noreply.github.com>
2023-05-25 13:37:51 +0000

    deploy: 8c979881bf227c53c158529d122763c7c83e3f5d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev39+g53be27b9',
+    VERSION: '0.2.4.dev40+g8c979881',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

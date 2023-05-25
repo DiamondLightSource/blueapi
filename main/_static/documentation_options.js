c41fe11b738e75365c2ddd4fd78c3c6fc28255e0
joeshannon <joeshannon@users.noreply.github.com>
2023-05-25 16:12:47 +0000

    deploy: ea63806858a54488ca309765feeb06899ec74cdf

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev42+g34a16fcf',
+    VERSION: '0.2.4.dev43+gea638068',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

e91921310c34423babea91b135cc95b459cb7397
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-05-30 13:32:45 +0000

    deploy: 7f138675f1750167c6b7312f01ea7ea85305308c

--- stop_and_abort/_static/documentation_options.js
+++ stop_and_abort/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.dev2+gebd68399',
+    VERSION: '0.3.1.dev2+g7f138675',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

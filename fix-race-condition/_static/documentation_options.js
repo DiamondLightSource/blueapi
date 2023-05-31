c8c23c9f26cebcac929b88c1d35e1f1ff815ec0f
callumforrester <callumforrester@users.noreply.github.com>
2023-05-31 15:00:39 +0000

    deploy: 31bacd15529e192da8c8a88b697a8f69c49e254f

--- fix-race-condition/_static/documentation_options.js
+++ fix-race-condition/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.dev7+g0737e15e',
+    VERSION: '0.3.1.dev8+g31bacd15',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

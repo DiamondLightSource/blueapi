f5e148be0450e97e6c9aa9314ca95f11ce0bfa1d
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-06-16 12:07:27 +0000

    deploy: fcffc06c86e0445e817e2fc61d8acdf3acf53ebe

--- stomp-correlation/_static/documentation_options.js
+++ stomp-correlation/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.2.dev13+ge6bcdffa',
+    VERSION: '0.3.2.dev14+gfcffc06c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

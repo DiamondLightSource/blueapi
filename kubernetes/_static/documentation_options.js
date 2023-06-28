e5e48eca49fdc15ee05e9361f6a87fab4fa27d47
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-06-28 12:04:57 +0000

    deploy: 7457c82a6bef2f82f114a2bdbfb86566e3e8e71a

--- kubernetes/_static/documentation_options.js
+++ kubernetes/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.3.dev5+gcd6d4587',
+    VERSION: '0.3.3.dev6+g7457c82a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

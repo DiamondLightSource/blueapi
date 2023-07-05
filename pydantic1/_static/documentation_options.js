ca2f504be5d06714daf6a98ce9c9a691f5aee40e
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-07-05 08:18:12 +0000

    deploy: 5a49f9f0025d637bafa8c78803d4555a9c61b871

---
+++ pydantic1/_static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.3.2.dev1+g5a49f9f0',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};

c9341a71e6272b96d10c8d14a9805209b05736d8
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-07-10 12:22:50 +0000

    deploy: 696a9c834fb756e8498ba102c660df53c6fcf6a7

--- pinnedPydantic/_static/documentation_options.js
+++ pinnedPydantic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.4.dev5+gbf1dd373',
+    VERSION: '0.3.4.dev6+g696a9c83',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

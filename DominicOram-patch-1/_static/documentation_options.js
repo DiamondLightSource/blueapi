542def9ef04c74a836a4cd5e90451877d5de46bf
DominicOram <DominicOram@users.noreply.github.com>
2023-07-24 14:15:17 +0000

    deploy: eca21f2077c16846ca57721edef8cf98a6c6d8cf

--- DominicOram-patch-1/_static/documentation_options.js
+++ DominicOram-patch-1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.5.dev1+g3a94c6e3',
+    VERSION: '0.3.5.dev1+geca21f20',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

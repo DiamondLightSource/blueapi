fccaab50731e0c631243453c81ab3affb491864a
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-08-02 16:51:15 +0000

    deploy: da6b1a1039e8455bf184619e52630e24f8973872

--- authn_authz/_static/documentation_options.js
+++ authn_authz/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.5.dev3+gb8009962',
+    VERSION: '0.3.5.dev4+gda6b1a10',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

502ad4ea615a116acae8c817b334f14075ad76c5
keithralphs <keithralphs@users.noreply.github.com>
2023-08-14 14:39:23 +0000

    deploy: a62f3a410eda733774b7cbd655343c3b8d019d15

--- 0.3.5-beta9/_static/documentation_options.js
+++ 0.3.5-beta9/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.5b8',
+    VERSION: '0.3.5b9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

f9ace01ec373dcee5d16d5aa878f8163d6845e99
callumforrester <callumforrester@users.noreply.github.com>
2023-08-22 08:30:15 +0000

    deploy: 27865c28e12c485088a1f197326c9e503785c1d0

--- plan-docs/_static/documentation_options.js
+++ plan-docs/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.5.dev5+g9fd8f39c',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.5.dev7+g27865c28',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

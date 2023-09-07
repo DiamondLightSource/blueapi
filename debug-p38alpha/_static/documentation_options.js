c08066604971705315a2393049c23e7a2c7c8449
keithralphs <keithralphs@users.noreply.github.com>
2023-09-07 11:49:16 +0000

    deploy: eeb44ac545e204182e2ee44cd9370540e3093f88

---
+++ debug-p38alpha/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.5b11.dev1+geeb44ac5',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};

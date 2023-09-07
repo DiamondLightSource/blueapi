04e378c493f8eb4e91ef1ae3e1c97e4218742c31
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-09-07 13:02:16 +0000

    deploy: c47a00e05dd86ead186da052f1af90b4010511e4

---
+++ preventIncompatibility/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.5.dev10+gc47a00e0',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};

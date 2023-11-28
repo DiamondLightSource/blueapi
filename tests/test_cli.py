5b264f5e89a0352b09d151abeb5afc2549068aef
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-11-28 15:37:16 +0000

    Add test for config printing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,6 +9,7 @@ from requests.exceptions import ConnectionError
 
 from blueapi import __version__
 from blueapi.cli.cli import main
+from blueapi.config import ApplicationConfig
 from blueapi.core.bluesky_types import Plan
 from blueapi.service.handler import Handler, teardown_handler
 
@@ -169,3 +170,42 @@ def test_config_passed_down_to_command_children(
             "params": {"time": 5},
         }
     }
+
+
+def test_print_config_prints_default_config_with_no_other_arguments(runner: CliRunner):
+    # Hard-coding the default config here means this test will fail if the default ever
+    # changes. This is not intended as a showstopper but just to prevent the default 
+    # changing without developer intention. If this test's assertion is failing for you 
+    # and you are okay with the default changing, run blueapi config and paste the output
+    # into this variable.
+    default_config = """api:
+  host: localhost
+  port: 8000
+  protocol: http
+env:
+  data_writing:
+    group_name: example
+    visit_directory: /tmp/0-0
+    visit_service_url: null
+  events:
+    broadcast_status_events: true
+  sources:
+  - kind: deviceFunctions
+    module: blueapi.startup.example_devices
+  - kind: planFunctions
+    module: blueapi.startup.example_plans
+  - kind: planFunctions
+    module: dls_bluesky_core.plans
+  - kind: planFunctions
+    module: dls_bluesky_core.stubs
+logging:
+  level: INFO
+stomp:
+  auth: null
+  host: localhost
+  port: 61613
+
+"""
+    result = runner.invoke(main, ["config"])
+    assert result.exit_code == 0
+    assert result.stdout == default_config

48f7d3aa23a30fb77050ce19165a6715237796b2
callumforrester <callumforrester@users.noreply.github.com>
2023-11-28 15:59:58 +0000

    deploy: 1f7ecfb30abb0024e89498a789b021d9f1cc1158

---
+++ remove-example-config/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.16.dev7+g1f7ecfb3',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};

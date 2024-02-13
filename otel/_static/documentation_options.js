a45ed9fbbfae4a5bdadcf4c621fd1533ae05ae79
keithralphs <keithralphs@users.noreply.github.com>
2024-02-13 15:09:40 +0000

    deploy: 08a539ba63a45d43bd9143c0917519792fe6b2c8

--- otel/_static/documentation_options.js
+++ otel/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.3.15.dev2+g7dba8107',
+    VERSION: '0.3.15a1.dev1+g08a539ba',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

d509dcdeb2137b0623117795836e71e15d91dc77
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2024-02-27 09:33:51 +0000

    deploy: 54a96a807709565039a2aae1fd24d0f987719a68

---
+++ writing-plans-readme/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.15a1.dev2+g54a96a80',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};

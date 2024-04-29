92788f53ce339c7a98704c6fdfd53eba0fd84b51
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-04-29 14:52:25 +0100

    got the script

--- generate-client.sh
+++ generate-client.sh
@@ -1,5 +1,12 @@
+
 podman run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i /local/docs/reference/schema-for-autogen.yaml \
   -g python-pydantic-v1 \
-  -o /local/src/clients/python
+  -o /local/tmp
+
+NEW_PATH=src/blueapi
+mv -f tmp/openapi_client $NEW_PATH
+
+echo "__all__ = [ApiClient]" > "$NEW_PATH"/openapi_client/__init__.py
+rm -rf tmp

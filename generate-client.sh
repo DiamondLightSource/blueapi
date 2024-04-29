ba7e668a3a8692bac9e4c8ea56bc32be284ee44b
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-04-29 18:04:48 +0100

    update the script to first remove the old folder

--- generate-client.sh
+++ generate-client.sh
@@ -6,6 +6,7 @@ podman run --rm \
   -o /local/tmp
 
 NEW_PATH=src/blueapi
+rm -rf "$NEW_PATH"/openapi_client
 mv -f tmp/openapi_client $NEW_PATH
 
 echo "__all__ = [ApiClient]" > "$NEW_PATH"/openapi_client/__init__.py

bfe9f4da595761ca33dff6eab4e700deca3de3c1
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-04-30 10:43:01 +0000

    fix init import

--- src/blueapi/openapi_client/__init__.py
+++ src/blueapi/openapi_client/__init__.py
@@ -20,29 +20,29 @@ __version__ = "1.0.0"
 from openapi_client.api.default_api import DefaultApi
 
 # import ApiClient
-from openapi_client.api_response import ApiResponse
-from openapi_client.api_client import ApiClient
-from openapi_client.configuration import Configuration
-from openapi_client.exceptions import OpenApiException
-from openapi_client.exceptions import ApiTypeError
-from openapi_client.exceptions import ApiValueError
-from openapi_client.exceptions import ApiKeyError
-from openapi_client.exceptions import ApiAttributeError
-from openapi_client.exceptions import ApiException
+from blueapi.openapi_client.api_response import ApiResponse
+from blueapi.openapi_client.api_client import ApiClient
+from blueapi.openapi_client.configuration import Configuration
+from blueapi.openapi_client.exceptions import OpenApiException
+from blueapi.openapi_client.exceptions import ApiTypeError
+from blueapi.openapi_client.exceptions import ApiValueError
+from blueapi.openapi_client.exceptions import ApiKeyError
+from blueapi.openapi_client.exceptions import ApiAttributeError
+from blueapi.openapi_client.exceptions import ApiException
 
 # import models into sdk package
-from openapi_client.models.device_model import DeviceModel
-from openapi_client.models.device_response import DeviceResponse
-from openapi_client.models.environment_response import EnvironmentResponse
-from openapi_client.models.http_validation_error import HTTPValidationError
-from openapi_client.models.location_inner import LocationInner
-from openapi_client.models.plan_model import PlanModel
-from openapi_client.models.plan_response import PlanResponse
-from openapi_client.models.state_change_request import StateChangeRequest
-from openapi_client.models.task import Task
-from openapi_client.models.task_response import TaskResponse
-from openapi_client.models.trackable_task import TrackableTask
-from openapi_client.models.validation_error import ValidationError
-from openapi_client.models.worker_state import WorkerState
-from openapi_client.models.worker_task import WorkerTask
+from blueapi.openapi_client.models.device_model import DeviceModel
+from blueapi.openapi_client.models.device_response import DeviceResponse
+from blueapi.openapi_client.models.environment_response import EnvironmentResponse
+from blueapi.openapi_client.models.http_validation_error import HTTPValidationError
+from blueapi.openapi_client.models.location_inner import LocationInner
+from blueapi.openapi_client.models.plan_model import PlanModel
+from blueapi.openapi_client.models.plan_response import PlanResponse
+from blueapi.openapi_client.models.state_change_request import StateChangeRequest
+from blueapi.openapi_client.models.task import Task
+from blueapi.openapi_client.models.task_response import TaskResponse
+from blueapi.openapi_client.models.trackable_task import TrackableTask
+from blueapi.openapi_client.models.validation_error import ValidationError
+from blueapi.openapi_client.models.worker_state import WorkerState
+from blueapi.openapi_client.models.worker_task import WorkerTask
 __all__ = [ApiClient]

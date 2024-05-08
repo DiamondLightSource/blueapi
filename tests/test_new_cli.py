98c1e68f02802858cdb6e9b1ba1089364ea3dbb1
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-08 14:57:20 +0100

    partial update to the tests

--- tests/test_new_cli.py
+++ tests/test_new_cli.py
@@ -67,109 +67,10 @@ class MyModel(BaseModel):
 class MyDevice:
     name: str
 
-
-@pytest.mark.handler
-@patch("blueapi.service.handler.Handler")
-@patch("requests.request")
-def test_get_plans_and_devices(
-    mock_requests: Mock,
-    mock_handler: Mock,
-    handler: Handler,
-    client: TestClient,
-    runner: CliRunner,
-):
-    """Integration test to test get_plans and get_devices."""
-
-    # needed so that the handler is instantiated as MockHandler() instead of Handler().
-    mock_handler.side_effect = Mock(return_value=handler)
-
-    # Setup the (Mock)Handler.
-    with patch("uvicorn.run", side_effect=None):
-        result = runner.invoke(main, ["serve"])
-
-    assert result.exit_code == 0
-
-    # Put a plan in handler.context manually.
-    plan = Plan(name="my-plan", model=MyModel)
-    handler._context.plans = {"my-plan": plan}
-
-    # Setup requests.get call to return the output of the FastAPI call for plans.
-    # Call the CLI function and check the output.
-    mock_requests.return_value = client.get("/plans")
-    plans = runner.invoke(main, ["controller", "plans"])
-
-    assert (
-        plans.output == "{'plans': [{'description': None,\n"
-        "            'name': 'my-plan',\n"
-        "            'parameter_schema': {'properties': {'id': {'title': 'Id',\n"
-        "                                                       'type': 'string'}},\n"
-        "                                 'required': ['id'],\n"
-        "                                 'title': 'MyModel',\n"
-        "                                 'type': 'object'}}]}\n"
-    )
-
-    # Setup requests.get call to return the output of the FastAPI call for devices.
-    # Call the CLI function and check the output - expect nothing as no devices set.
-    handler._context.devices = {}
-    mock_requests.return_value = client.get("/devices")
-    unset_devices = runner.invoke(main, ["controller", "devices"])
-    assert unset_devices.output == "{'devices': []}\n"
-
-    # Put a device in handler.context manually.
-    device = MyDevice("my-device")
-    handler._context.devices = {"my-device": device}
-
-    # Setup requests.get call to return the output of the FastAPI call for devices.
-    # Call the CLI function and check the output.
-    mock_requests.return_value = client.get("/devices")
-    devices = runner.invoke(main, ["controller", "devices"])
-
-    assert (
-        devices.output
-        == "{'devices': [{'name': 'my-device', 'protocols': ['HasName']}]}\n"
-    )
-
-
 def test_invalid_config_path_handling(runner: CliRunner):
     # test what happens if you pass an invalid config file...
     result = runner.invoke(main, ["-c", "non_existent.yaml"])
     assert result.exit_code == 1
-
-
-@pytest.mark.handler
-@patch("blueapi.service.handler.Handler")
-@patch("requests.request")
-def test_config_passed_down_to_command_children(
-    mock_requests: Mock,
-    mock_handler: Mock,
-    handler: Handler,
-    runner: CliRunner,
-):
-    mock_handler.side_effect = Mock(return_value=handler)
-    config_path = "tests/example_yaml/rest_config.yaml"
-
-    with patch("uvicorn.run", side_effect=None):
-        result = runner.invoke(main, ["-c", config_path, "serve"])
-
-    assert result.exit_code == 0
-
-    mock_requests.return_value = Mock()
-
-    runner.invoke(
-        main, ["-c", config_path, "controller", "run", "sleep", '{"time": 5}']
-    )
-
-    assert mock_requests.call_args[0] == (
-        "POST",
-        "http://a.fake.host:12345/tasks",
-    )
-    assert mock_requests.call_args[1] == {
-        "json": {
-            "name": "sleep",
-            "params": {"time": 5},
-        }
-    }
-
 
 def test_invalid_stomp_config_for_listener(runner: CliRunner):
     result = runner.invoke(main, ["controller", "listen"])
@@ -211,7 +112,9 @@ def test_invalid_condition_for_run(runner: CliRunner):
 @pytest.mark.handler
 @patch("blueapi.service.handler.Handler")
 @patch("urllib3.PoolManager.request")
-def test_get_devices_2(mock_requests: Mock, mock_handler: Mock, runner: CliRunner):
+def test_get_devices_with_custom_config(
+    mock_requests: Mock, mock_handler: Mock, runner: CliRunner
+):
     # Setup a mock response
     mock_response = MagicMock()
     mock_response.status = 200  # expect a successful response
@@ -228,6 +131,10 @@ def test_get_devices_2(mock_requests: Mock, mock_handler: Mock, runner: CliRunne
 
     mock_handler._context.devices = {}
 
+    # # Put a device in handler.context manually.
+    # device = MyDevice("my-device")
+    # mock_handler._context.devices = {"my-device": device}
+
     response = runner.invoke(main, ["-c", config_path, "controller", "devices"])
     print(response)
     mock_requests.assert_called_once_with(
@@ -241,3 +148,56 @@ def test_get_devices_2(mock_requests: Mock, mock_handler: Mock, runner: CliRunne
             "User-Agent": "OpenAPI-Generator/1.0.0/python",
         },
     )
+
+
+@pytest.mark.handler
+@patch("blueapi.service.handler.Handler")
+@patch("urllib3.PoolManager.request")
+def test_get_plans_with_custom_config(
+    mock_requests: Mock, mock_handler: Mock, runner: CliRunner
+):
+    # Setup a mock response
+    mock_response = MagicMock()
+    mock_response.status = 200  # expect a successful response
+    mock_response.data = b'{"plans": []}'  # Mock a JSON response
+
+    # Put a plan in handler.context manually.
+
+    config_path = "tests/example_yaml/rest_config.yaml"
+
+    with patch("uvicorn.run", side_effect=None):
+        initial_result = runner.invoke(main, ["-c", config_path, "serve"])
+        print(initial_result)
+
+    # Configure the mock to return the response
+    mock_requests.return_value = mock_response
+
+    plan = Plan(name="my-plan", model=MyModel)
+    mock_handler._context.plans = {"my-plan": plan}
+    plans = runner.invoke(main, ["-c", config_path, "controller", "plans"])
+
+    assert (
+        plans.output == "{'plans': [{'description': None,\n"
+        "            'name': 'my-plan',\n"
+        "            'parameter_schema': {'properties': {'id': {'title': 'Id',\n"
+        "                                                       'type': 'string'}},\n"
+        "                                 'required': ['id'],\n"
+        "                                 'title': 'MyModel',\n"
+        "                                 'type': 'object'}}]}\n"
+    )
+
+    mock_requests.assert_called_once_with(
+        "GET",
+        "http://a.fake.host:12345/plans",
+        fields={},
+        preload_content=True,
+        timeout=None,
+        headers={
+            "Accept": "application/json",
+            "User-Agent": "OpenAPI-Generator/1.0.0/python",
+        },
+    )
+    # todo set plan with sleep args
+    # runner.invoke(
+    #     main, ["-c", config_path, "controller", "run", "sleep", '{"time": 5}']
+    # )

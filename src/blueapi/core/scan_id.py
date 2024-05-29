aa8e14bd5383ac882f5e2f036ad1e7ec990266d0
Callum Forrester <callum.forrester@diamond.ac.uk>
2024-05-29 10:56:03 +0100

    GDA client

---
+++ src/blueapi/core/scan_id.py
@@ -0,0 +1,41 @@
+import uuid
+from abc import ABC, abstractmethod
+
+import requests
+from pydantic import BaseModel
+
+
+class UniqueScanIdSource(ABC):
+    @abstractmethod
+    def get_new_scan_id(self) -> int | str:
+        """Generate a unique new ID for a run
+
+        Returns:
+            int | str: A unique ID for the scan
+        """
+
+
+class UuidScanIdSource(UniqueScanIdSource):
+    def get_new_scan_id(self) -> int | str:
+        return str(uuid.uuid4())
+
+
+class GdaNumtrackerResponse(BaseModel):
+    """
+    Equivalent to a `Scan Number` or `scan_id`, non-globally unique scan identifier.
+    Should be always incrementing, unique per-visit, co-ordinated with any other scan engines.
+    """
+
+    collectionNumber: int
+
+
+class GdaScanIdSource(UniqueScanIdSource):
+    def __init__(self, url: str) -> None:
+        self._url = url
+        super().__init__()
+
+    def get_new_scan_id(self) -> int | str:
+        response = requests.post(f"{self._url}/numtracker")
+        response.raise_for_status()
+        gda_response = GdaNumtrackerResponse.parse_obj(response.json())
+        return gda_response.collectionNumber

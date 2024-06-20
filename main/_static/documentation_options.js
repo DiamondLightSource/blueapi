88897a65a9ae81c686125d6c643b72ad0ae66180
stan-dot <stan-dot@users.noreply.github.com>
2024-06-20 09:08:18 +0000

    deploy: 31c94c5d054e86f22047553e97ad440e954e68be

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.4.3rc2.dev2+gd18fcf81',
+    VERSION: '0.4.3rc2.dev3+g31c94c5d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

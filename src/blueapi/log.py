fc51986dc2c6482c5ed8ca6f5aa8211246a23101
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-09-19 11:55:28 +0100

    Create log.py with basic Graylog support

---
+++ src/blueapi/log.py
@@ -0,0 +1,21 @@
+import logging
+
+from dodal.log import LOGGER as dodal_logger
+from dodal.log import (
+    get_graylog_configuration,
+    set_up_graylog_handler,
+)
+
+from blueapi.config import LoggingConfig
+
+logging_config = LoggingConfig()
+
+LOGGER = logging.getLogger("blueapi")
+LOGGER.setLevel(logging_config.level)
+LOGGER.parent = dodal_logger
+
+
+def do_default_logging_setup(dev_mode=False):
+    set_up_graylog_handler(
+        LOGGER, *get_graylog_configuration(dev_mode, logging_config.graylog_port)
+    )

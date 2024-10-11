00518c1439c3c2d2103d6317fcbe47aedb244dcd
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-10-11 15:14:53 +0000

    use rabbitmq instead

--- src/script/start_rabbitmq.sh
+++ src/script/start_rabbitmq.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-echo "Checking docker installation"
+cmd1='build -t rabbitmq-stomp ./rabbitmq_setup/.'
+cmd2='run -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp'
+
+echo "Checking docker/podman installation"
 if command -v docker &> /dev/null; then
-    docker run -it --rm --net host rmohr/activemq:5.15.9-alpine
+    docker $cmd1
+    docker $cmd2
 elif command -v podman &> /dev/null; then
-    podman run -it --rm --net host rmohr/activemq:5.15.9-alpine
+    podman $cmd1
+    podman $cmd2
 else
-    echo "Docker and Podman installation not found. Please install docker/podman."
+    echo "Docker/Podman installation not found. Please install docker/podman."
     exit 1
 fi

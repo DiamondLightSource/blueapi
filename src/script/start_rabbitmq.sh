86c10b51774267b115cef55f2cf091bd6165d951
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-11-04 12:43:39 +0000

    removed dockerfile and added enableed_pligins file

--- src/script/start_rabbitmq.sh
+++ src/script/start_rabbitmq.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "$BASH_SOURCE[0]" )" &> /dev/null && pwd )
-cmd1='build -t rabbitmq-stomp '$SCRIPT_DIR'/rabbitmq_setup/.'
-cmd2='run -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp'
+RABBITMQ_VERSION="rabbitmq:3.13.2-management"
+cmd1='run -it --rm --name rabbitmq -v '$SCRIPT_DIR'/rabbitmq_setup/rabbitmq.conf:/etc/rabbitmq/rabbitmq.conf -v '\
+$SCRIPT_DIR'/rabbitmq_setup/enabled_plugins:/etc/rabbitmq/enabled_plugins'\
+' -p 5672:5672 -p 15672:15672 -p 61613:61613 '$RABBITMQ_VERSION
 
 echo "Checking docker/podman installation"
 if command -v docker &> /dev/null; then
     docker $cmd1
-    docker $cmd2
 elif command -v podman &> /dev/null; then
     podman $cmd1
-    podman $cmd2
 else
     echo "Docker/Podman installation not found. Please install docker/podman."
     exit 1

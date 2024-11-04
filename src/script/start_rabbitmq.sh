4de19518ea2b8f215b4afeeaced32ac475a5fd76
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-11-04 15:01:33 +0000

    remove rabbitmq config and version pinning

--- src/script/start_rabbitmq.sh
+++ src/script/start_rabbitmq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "$BASH_SOURCE[0]" )" &> /dev/null && pwd )
-RABBITMQ_VERSION="rabbitmq:3.13.2-management"
-cmd1='run -it --rm --name rabbitmq -v '$SCRIPT_DIR'/rabbitmq_setup/rabbitmq.conf:/etc/rabbitmq/rabbitmq.conf -v '\
+RABBITMQ_VERSION="rabbitmq:management"
+cmd1='run -it --rm --name rabbitmq -v '\
 $SCRIPT_DIR'/rabbitmq_setup/enabled_plugins:/etc/rabbitmq/enabled_plugins'\
 ' -p 5672:5672 -p 15672:15672 -p 61613:61613 '$RABBITMQ_VERSION
 

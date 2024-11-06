999c450f9972db6535399cd7e2730678c59985b6
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-06 15:06:39 +0000

    Add stream handler mock fixture

--- tests/unit_tests/test_log.py
+++ tests/unit_tests/test_log.py
@@ -1,4 +1,3 @@
-import logging
 from unittest.mock import patch
 
 import pytest
@@ -7,13 +6,21 @@ from blueapi.log import LOGGER, do_default_logging_setup
 
 
 @pytest.fixture
-def mock_loggers():
+def mock_graylog_emit():
     with patch("dodal.log.GELFTCPHandler.emit") as graylog_emit:
         graylog_emit.reset_mock()
         yield graylog_emit
 
 
-def test_logger_emits(mock_loggers):
+@pytest.fixture
+def mock_stream_handler_emit():
+    with patch("dodal.log.StreamHandler.emit") as stream_handler_emit:
+        stream_handler_emit.reset_mock()
+        yield stream_handler_emit
+
+
+def test_logger_emits(mock_graylog_emit):
+    # def test_logger_emits(mock_handlers):
     do_default_logging_setup(dev_mode=True)
     LOGGER.info("FOO")
-    mock_loggers.assert_called()
+    mock_graylog_emit.assert_called()

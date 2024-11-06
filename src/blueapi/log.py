3c7786096ca18422821bf0a0ecfc00298aabb997
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-06 11:39:30 +0000

    Add stream handler to logging

--- src/blueapi/log.py
+++ src/blueapi/log.py
@@ -4,6 +4,7 @@ from dodal.log import LOGGER as dodal_logger
 from dodal.log import (
     get_graylog_configuration,
     set_up_graylog_handler,
+    set_up_stream_handler,
 )
 
 from blueapi.config import LoggingConfig
@@ -16,6 +17,7 @@ LOGGER.parent = dodal_logger
 
 
 def do_default_logging_setup(dev_mode=False):
+    set_up_stream_handler(LOGGER)
     set_up_graylog_handler(
         LOGGER, *get_graylog_configuration(dev_mode, logging_config.graylog_port)
     )

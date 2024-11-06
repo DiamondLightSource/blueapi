5feeb8ef3eff36f9d3bff821e6e2e7276e2b4824
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-06 14:38:09 +0000

    Move handlers into global __logger_handlers

--- src/blueapi/log.py
+++ src/blueapi/log.py
@@ -1,7 +1,9 @@
 import logging
+from typing import Optional
 
 from dodal.log import LOGGER as dodal_logger
 from dodal.log import (
+    DodalLogHandlers,
     get_graylog_configuration,
     set_up_graylog_handler,
     set_up_stream_handler,
@@ -15,6 +17,8 @@ LOGGER = logging.getLogger("blueapi")
 LOGGER.setLevel(logging_config.level)
 LOGGER.parent = dodal_logger
 
+__logger_handlers: DodalLogHandlers | None = None
+
 
 def do_default_logging_setup(dev_mode=False):
     handlers = {}
@@ -22,4 +26,5 @@ def do_default_logging_setup(dev_mode=False):
     handlers["graylog_handler"] = set_up_graylog_handler(
         LOGGER, *get_graylog_configuration(dev_mode, logging_config.graylog_port)
     )
-    return handlers
+    global __logger_handlers
+    __logger_handlers = handlers

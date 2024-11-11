5f5b35539e208e951ff6fb23941665a3e7290d4b
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-11-11 14:34:46 +0000

    Revert unrelated changes

--- src/script/start_rabbitmq.sh
+++ src/script/start_rabbitmq.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "$BASH_SOURCE[0]" )" &> /dev/null && pwd )
+RABBITMQ_VERSION="rabbitmq:management"
+cmd1='run -it --rm --name rabbitmq -v '\
+$SCRIPT_DIR'/rabbitmq_setup/enabled_plugins:/etc/rabbitmq/enabled_plugins'\
+' -p 5672:5672 -p 15672:15672 -p 61613:61613 '$RABBITMQ_VERSION
 
 echo "Checking docker/podman installation"
 if command -v docker &> /dev/null; then
-    docker compose -f $SCRIPT_DIR/docker-compose.yml up
+    docker $cmd1
 elif command -v podman &> /dev/null; then
-    podman compose -f $SCRIPT_DIR/docker-compose.yml up
+    podman $cmd1
 else
     echo "Docker/Podman installation not found. Please install docker/podman."
     exit 1

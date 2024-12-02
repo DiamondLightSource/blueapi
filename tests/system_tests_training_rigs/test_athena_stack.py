86d8b2897d969bd921d256c09d3bbff53d075f36
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2024-12-02 18:34:45 +0000

    added system test for p46

--- tests/system_tests_training_rigs/test_athena_stack.py
+++ tests/system_tests_training_rigs/test_athena_stack.py
@@ -3,6 +3,8 @@ from bluesky_stomp.models import BasicAuthentication
 
 from blueapi.client.client import BlueapiClient
 from blueapi.config import ApplicationConfig, RestConfig, StompConfig
+from blueapi.worker.event import WorkerState
+from blueapi.worker.task import Task
 
 
 @pytest.fixture
@@ -10,12 +12,9 @@ def training_rig_config() -> ApplicationConfig:
     return ApplicationConfig(
         stomp=StompConfig(
             host="localhost",
-            port=123,
             auth=BasicAuthentication(username="guest", password="guest"),  # type: ignore
         ),
-        api=RestConfig(
-            host="https://p46-blueapi.diamond.ac.uk/", port=80, protocol="https"
-        ),
+        api=RestConfig(host="p46-blueapi.diamond.ac.uk", port=443, protocol="https"),
     )
 
 
@@ -26,3 +25,49 @@ def client(training_rig_config) -> BlueapiClient:
 
 def test_get_plans(client: BlueapiClient):
     assert client.get_plans()
+
+
+SPEC_SCAN = Task(
+    name="spec_scan",
+    params={
+        "detectors": ["det"],
+        "spec": {
+            "axis": "sample_stage.x",
+            "start": 1.0,
+            "stop": 10.0,
+            "num": 10,
+            "type": "Line",
+        },
+    },
+)
+STEP_SCAN = Task(
+    name="plan_step_scan",
+    params={
+        "detectors": ["det"],
+        "motor": "sample_stage",
+    },
+)
+
+
+def test_spec_scan_task(client: BlueapiClient, plan: str = "spec_scan"):
+    assert client.get_plan(plan), f"In {plan} is available"
+
+    assert client.create_and_start_task(SPEC_SCAN)
+
+    assert (
+        client.get_state() == WorkerState.IDLE
+    )  # This will not be idle if the plan was working
+
+    ## Add checks for file creation
+
+
+def test_step_scan_task(client: BlueapiClient, plan: str = "plan_step_scan"):
+    assert client.get_plan(plan), f"In {plan} is available"
+
+    assert client.create_and_start_task(STEP_SCAN)
+
+    assert (
+        client.get_state() == WorkerState.IDLE
+    )  # This will not be idle if the plan was working
+
+    ## Add checks for file creation

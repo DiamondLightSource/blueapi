9c5accf5454fbab8fb4b0f0b51306ae1b01f6b2c
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-02-28 17:25:27 +0000

    Get required metadata from config into the run engine md

--- src/blueapi/core/context.py
+++ src/blueapi/core/context.py
@@ -106,6 +106,7 @@ class BlueskyContext:
             return find_component(self.devices, addr)
 
     def with_config(self, config: EnvironmentConfig) -> None:
+        self.run_engine.md |= config.metadata.model_dump()
         for source in config.sources:
             mod = import_module(str(source.module))
 

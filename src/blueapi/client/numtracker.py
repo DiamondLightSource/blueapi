3a01dfcdadf8442baee2dec677705c8aec427bca
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-03-18 16:34:17 +0000

    Add docstrings to numtracker client

--- src/blueapi/client/numtracker.py
+++ src/blueapi/client/numtracker.py
@@ -10,22 +10,39 @@ from blueapi.utils import BlueapiBaseModel
 
 
 class DirectoryPath(BlueapiBaseModel):
+    """
+    Directory location and associated metadata
+    """
+
     instrument: str
     instrument_session: str = Field(alias="instrumentSession")
     path: Path
 
 
 class ScanPaths(BlueapiBaseModel):
+    """
+    Description of where to write scan data
+    """
+
     scan_file: str = Field(alias="scanFile")
     scan_number: int = Field(alias="scanNumber")
     directory: DirectoryPath
 
 
 class NumtrackerScanMutationResponse(BlueapiBaseModel):
+    """
+    Response from numtracker server when creating a new scan
+    """
+
     scan: ScanPaths
 
 
 class NumtrackerClient:
+    """
+    Client for numtracker: https://github.com/DiamondLightSource/numtracker
+    Can be used to assign new scan numbers.
+    """
+
     def __init__(
         self,
         url: str,
@@ -34,12 +51,29 @@ class NumtrackerClient:
         self._url = url
         self._headers = headers
 
-    def set_headers(self, pass_through_headers: Mapping[str, str]) -> None:
-        self._headers = pass_through_headers
+    def set_headers(self, headers: Mapping[str, str]) -> None:
+        """
+        Set default HTTP headers
+
+        Args:
+            headers: headers to embed in every request.
+        """
+
+        self._headers = headers
 
     def create_scan(
         self, instrument_session: str, instrument: str
     ) -> NumtrackerScanMutationResponse:
+        """
+        Create a new scan with numtracker.
+
+        Args:
+            instrument_session: The proposal number, proposal code and visit ID
+            e.g. cm12345-1
+
+            instrument: The instrument to write data on e.g. i22
+        """
+
         query = {
             "query": dedent(f"""
             mutation{{

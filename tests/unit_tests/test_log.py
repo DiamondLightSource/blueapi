3245973af6dfaf71a3da65b21d19afdea3e72fdb
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-03-19 15:23:11 +0000

    Extract test logger into fixtures

--- tests/unit_tests/test_log.py
+++ tests/unit_tests/test_log.py
@@ -6,13 +6,35 @@ import pytest
 from blueapi.config import LoggingConfig
 from blueapi.log import do_default_logging_setup
 
-LOGGER = logging.getLogger("blueapi")
 
-
-def clear_all_loggers_and_handlers():
-    for handler in LOGGER.handlers:
+def clear_all_loggers_and_handlers(logger):
+    for handler in logger.handlers:
         handler.close()
-    LOGGER.handlers.clear()
+    logger.handlers.clear()
+
+
+LOGGER_NAMES = ["blueapi", "blueapi.test"]
+
+
+@pytest.fixture(params=LOGGER_NAMES)
+def logger_with_graylog(request):
+    logger = logging.getLogger(request.param)
+    do_default_logging_setup(LoggingConfig(graylog_export_enabled=True))
+    yield logger
+    clear_all_loggers_and_handlers(logger)
+
+
+@pytest.fixture(params=LOGGER_NAMES)
+def logger_without_graylog(request):
+    logger = logging.getLogger(request.param)
+    do_default_logging_setup(LoggingConfig(graylog_export_enabled=False))
+    yield logger
+    clear_all_loggers_and_handlers(logger)
+
+
+@pytest.fixture
+def logger(logger_with_graylog):
+    return logger_with_graylog
 
 
 @pytest.fixture
@@ -23,15 +45,8 @@ def mock_graylog_emit():
 
 
 @pytest.fixture
-def mock_logger_config():
-    with patch("blueapi.log.LoggingConfig") as mock_logging_config:
-        logger_config = LoggingConfig()
-
-        logger_config.graylog_export_enabled = False
-
-        mock_logging_config.return_value = logger_config
-
-        yield logger_config
+def mock_logger_config() -> LoggingConfig:
+    return LoggingConfig(graylog_export_enabled=False)
 
 
 @pytest.fixture
@@ -41,41 +56,34 @@ def mock_stream_handler_emit():
         yield stream_handler_emit
 
 
-def test_logger_emits_to_graylog(mock_graylog_emit):
-    do_default_logging_setup(dev_mode=True)
-    LOGGER.info("FOO")
+def test_logger_emits_to_graylog(logger_with_graylog, mock_graylog_emit):
+    logger_with_graylog.info("FOO")
     mock_graylog_emit.assert_called()
 
 
-def test_logger_does_not_emit_to_graylog(mock_graylog_emit, mock_logger_config):
-    clear_all_loggers_and_handlers()
-    do_default_logging_setup(dev_mode=False)
-    LOGGER.info("FOO")
+def test_logger_does_not_emit_to_graylog(logger_without_graylog, mock_graylog_emit):
+    logger_without_graylog.info("FOO")
     mock_graylog_emit.assert_not_called()
-    clear_all_loggers_and_handlers()
 
 
 def test_graylog_formats_to_json():
     raise NotImplementedError()
 
 
-def test_stream_handler_emits(mock_stream_handler_emit):
-    do_default_logging_setup(dev_mode=True)
-    LOGGER.info("FOO")
+def test_stream_handler_emits(logger, mock_stream_handler_emit):
+    logger.info("FOO")
     mock_stream_handler_emit.assert_called()
 
 
-def test_messages_are_tagged_with_beamline(mock_stream_handler_emit):
-    do_default_logging_setup(dev_mode=True)
-    LOGGER.info("FOO")
+def test_messages_are_tagged_with_beamline(logger, mock_stream_handler_emit):
+    logger.info("FOO")
     assert mock_stream_handler_emit.call_args[0][0].beamline == "dev"
 
 
-def test_messages_are_tagged_with_instrument(mock_stream_handler_emit):
-    do_default_logging_setup(dev_mode=True)
-    LOGGER.info("FOO")
+def test_messages_are_tagged_with_instrument(logger, mock_stream_handler_emit):
+    logger.info("FOO")
     assert mock_stream_handler_emit.call_args[0][0].instrument == "dev"
 
 
-def test_messages_are_tagged_with_plan_name(mock_stream_handler_emit):
+def test_messages_are_tagged_with_plan_name(logger, mock_stream_handler_emit):
     raise NotImplementedError()

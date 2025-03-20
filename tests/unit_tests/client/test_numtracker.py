d9ade2317ac77fc0f0eddd5be1dd62d70bc2f423
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-03-20 15:05:57 +0000

    Add test for numtracker client create scan

--- tests/unit_tests/client/test_numtracker.py
+++ tests/unit_tests/client/test_numtracker.py
@@ -1,5 +1,34 @@
+from pathlib import Path
+
+import pytest
+import responses
+
+from blueapi.client.numtracker import (
+    DirectoryPath,
+    NumtrackerClient,
+    NumtrackerScanMutationResponse,
+    ScanPaths,
+)
 
 
-def test_create_scan():
-    ...
+@pytest.fixture
+def numtracker() -> NumtrackerClient:
+    return NumtrackerClient("https://numtracker-example.com/graphql", {})
 
+
+def test_create_scan(
+    numtracker: NumtrackerClient,
+    mock_numtracker_server: responses.RequestsMock,
+):
+    scan = numtracker.create_scan("cm12345-1", "i22")
+    assert scan == NumtrackerScanMutationResponse(
+        scan=ScanPaths(
+            scanFile="p46-11",
+            scanNumber=11,
+            directory=DirectoryPath(
+                instrument="p46",
+                instrumentSession="p46-1",
+                path=Path("/exports/mybeamline/data/2025"),
+            ),
+        )
+    )

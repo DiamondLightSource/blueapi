80d27404ba14596b5141937b83a814360e156f14
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-03-20 15:55:40 +0000

    Fix docstrings and type hinting

--- src/blueapi/log.py
+++ src/blueapi/log.py
@@ -7,6 +7,12 @@ from blueapi.config import LoggingConfig
 
 
 class BeamlineTagFilter(logging.Filter):
+    """Filter to attach beamline attribute to LogRecords.
+
+    Attaches attribute `beamline` to all LogRecords that are passed through. Value is
+    taken from `BEAMLINE` env var, defaulting to `dev`.
+    """
+
     beamline: str | None = os.environ.get("BEAMLINE")
 
     def filter(self, record):
@@ -15,6 +21,11 @@ class BeamlineTagFilter(logging.Filter):
 
 
 class InstrumentTagFilter(logging.Filter):
+    """Filter to attach instrument attribute to LogRecords.
+
+    Attaches attribute `instrument` to all LogRecords that are passed through. Value
+    is taken from `BEAMLINE` env var, defaulting to `dev`."""
+
     instrument: str | None = os.environ.get("BEAMLINE")
 
     def filter(self, record):
@@ -54,14 +65,30 @@ def setup_logging(logging_config: LoggingConfig) -> None:
     add_all_filters_to_all_handlers(handlers, filters)
 
 
-def set_up_stream_handler(logger: logging.Logger, logging_config: LoggingConfig):
+def set_up_stream_handler(
+    logger: logging.Logger, logging_config: LoggingConfig
+) -> logging.StreamHandler:
+    """Creates and configures StreamHandler, then attaches to logger.
+
+    Args:
+        logger: Logger to attach handler to
+        logging_config: LoggingConfig
+    """
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging_config.level)
     logger.addHandler(stream_handler)
     return stream_handler
 
 
-def set_up_graylog_handler(logger: logging.Logger, logging_config: LoggingConfig):
+def set_up_graylog_handler(
+    logger: logging.Logger, logging_config: LoggingConfig
+) -> GELFTCPHandler:
+    """Creates and configures GELFTCPHandler, then attaches to logger.
+
+    Args:
+        logger: Logger to attach handler to
+        logging_config: LoggingConfig
+    """
     if logging_config.logging_dev_mode:
         graylog_handler = GELFTCPHandler("localhost", 5555)
     else:
@@ -75,7 +102,13 @@ def set_up_graylog_handler(logger: logging.Logger, logging_config: LoggingConfig
 
 def add_all_filters_to_all_handlers(
     handlers: list[logging.Handler], filters: list[logging.Filter]
-):
+) -> None:
+    """Attach all filters to each given handler.
+
+    Args:
+        handlers: list of Handlers to attach filters to
+        filters: list of Filters to attach to handlers
+    """
     for handler in handlers:
         for filter in filters:
             handler.addFilter(filter)

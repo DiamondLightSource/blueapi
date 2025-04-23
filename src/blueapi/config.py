04f434d4ac49e23fcf321515cb98273b7ababc27
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-04-23 13:32:02 +0000

    Add descriptions to other stomp config fields

--- src/blueapi/config.py
+++ src/blueapi/config.py
@@ -44,9 +44,12 @@ class StompConfig(BlueapiBaseModel):
         "event publishing",
         default=False,
     )
-    host: str = "localhost"
-    port: int = 61613
-    auth: BasicAuthentication | None = None
+    host: str = Field(description="STOMP broker host", default="localhost")
+    port: int = Field(description="STOMP broker port", default=61613)
+    auth: BasicAuthentication | None = Field(
+        description="Auth information for communicating with STOMP broker, if required",
+        default=None,
+    )
 
 
 class WorkerEventConfig(BlueapiBaseModel):

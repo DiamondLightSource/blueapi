f057388164159533bcc57595b956b72c78b434ab
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2025-05-28 10:12:39 +0100

    WIP clean up

--- clean-namespace.sh
+++
@@ -1,8 +0,0 @@
-kubectl delete all --all -n ckk43848
-kubectl delete configmap --all -n ckk43848
-kubectl delete secret --all -n ckk43848
-kubectl delete pvc --all -n ckk43848
-kubectl delete job --all -n ckk43848
-kubectl delete cronjob --all -n ckk43848
-kubectl delete ingress --all -n ckk43848
-kubectl delete serviceaccount --all -n ckk43848

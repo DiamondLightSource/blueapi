2c72e40faa19daf9b148b454bdd7e80aea45c97c
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2025-07-04 15:24:17 +0000

    deploy: c141d50653d139f1fa0e141f7019ba652a560435

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.0rc2.dev4+g3b7346233',
+    VERSION: '1.0.0rc2.dev5+gc141d5065',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

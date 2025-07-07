38e6599d02f964ff8f77867dca9a045380735260
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-07-07 08:19:49 +0000

    Change config_schema.json output location, add helm-schema to pre-commit

--- src/script/export_config_schema.py
+++ src/script/export_config_schema.py
@@ -4,7 +4,9 @@ from blueapi.config import ApplicationConfig
 
 
 def print_application_config_json_schema():
-    print(json.dumps(ApplicationConfig.model_json_schema()))
+    # print(json.dumps(ApplicationConfig.model_json_schema()))
+    with open("helm/blueapi/config_schema.json", "w") as file:
+        file.write(json.dumps(ApplicationConfig.model_json_schema()))
 
 
 if __name__ == "__main__":

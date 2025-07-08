59c74ef923b5664de3cbf01f4db5398446619bb7
tpoliaw <tpoliaw@users.noreply.github.com>
2025-07-08 14:40:15 +0000

    deploy: fa4282fc22fbc9f178e2e08d5a11fd4011082225

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.0rc3.dev3+gbec4e117b',
+    VERSION: '1.0.0rc3.dev4+gfa4282fc2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

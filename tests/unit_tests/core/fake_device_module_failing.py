f0e560e67a3619e5b7614d031426c8b17578fb98
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-08-01 08:46:51 +0000

    Create failing test for devices that fail to connect being included in context

--- tests/unit_tests/core/fake_device_module_failing.py
+++ tests/unit_tests/core/fake_device_module_failing.py
@@ -1,5 +1,20 @@
+from ophyd_async.core import LazyMock, StandardReadable
 from ophyd_async.epics.motor import Motor
+
+
+class TimingOutDevice(StandardReadable):
+    async def connect(
+        self,
+        mock: bool | LazyMock = False,
+        timeout: float = 1.0,
+        force_reconnect: bool = False,
+    ) -> None:
+        raise TimeoutError("Connection timed out")
 
 
 def failing_device() -> Motor:
     raise TimeoutError("FooBar")
+
+
+def timing_out_device() -> TimingOutDevice:
+    return TimingOutDevice()

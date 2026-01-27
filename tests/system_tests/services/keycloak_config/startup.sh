827cf75122e582b984d8328f16d9bbc46c3d758e
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2026-01-27 14:39:01 +0000

    Update client creation in keycloak

--- tests/system_tests/services/keycloak_config/startup.sh
+++ tests/system_tests/services/keycloak_config/startup.sh
@@ -1,151 +1,69 @@
 #!/bin/bash
 export PATH=$PATH:/opt/keycloak/bin
 
+# --- Config ---
+export KC_CLI_PASSWORD="admin"
+SERVER="http://localhost:8080"
+TEMPLATE="/tmp/config/mappers-template.json"
+
+# Wait for Keycloak
 sleep 30
-while ! kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin; do
-    sleep 1
+until kcadm.sh config credentials --server $SERVER --realm master --user admin; do sleep 1; done
+
+# Cleanup logic
+for type in "Allowed Protocol Mapper Types" "Allowed Client Scopes"; do
+    for id in $(kcadm.sh get components -q name="$type" --fields id --format csv --noquotes); do
+        kcadm.sh delete components/$id
+    done
 done
 
-allowed_protocol_mappers=$(kcadm.sh get components -q name="Allowed Protocol Mapper Types" --fields id --format csv --noquotes)
-allowed_client_scopes=$(kcadm.sh get components -q name="Allowed Client Scopes" --fields id --format csv --noquotes)
-for i in $allowed_protocol_mappers $allowed_client_scopes;do
-  kcadm.sh delete components/$i
-done
+kcreg.sh config credentials --server $SERVER --realm master --user admin
 
-kcreg.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
+# --- Client Creation Function ---
+# Args: client_id, audience, extra_flags
+create_client() {
+    local client_id=$1
+    local aud=$2
+    shift 2 # The rest are Keycloak attributes (-s key=value)
 
-protocolMappers='{
-  "protocolMappers": [
-    {
-      "name": "fedid",
-      "protocol": "openid-connect",
-      "protocolMapper": "oidc-hardcoded-claim-mapper",
-      "config": {
-        "introspection.token.claim": "true",
-        "claim.value": "alice",
-        "userinfo.token.claim": "true",
-        "id.token.claim": "true",
-        "access.token.claim": "true",
-        "claim.name": "fedid",
-        "jsonType.label": "String"
-      }
-    },
-    {
-      "name": "blueapi",
-      "protocol": "openid-connect",
-      "protocolMapper": "oidc-audience-mapper",
-      "config": {
-        "introspection.token.claim": "true",
-        "access.token.claim": "true",
-        "included.custom.audience": "blueapi"
-      }
-    }
-  ]
-}'
+    if kcreg.sh get "$client_id" >/dev/null 2>&1; then
+        echo ">> Skipping $client_id (exists)"
+        return
+    fi
 
+    echo ">> Creating $client_id..."
+    local tmpfile=$(mktemp)
 
-for client in "system-test-blueapi" "ixx-cli-blueapi" "ixx-blueapi" "tiled" "tiled-cli"; do
-  if ! kcreg.sh get "$client" >/dev/null 2>&1; then
-      tmpfile=$(mktemp)
-      echo $protocolMappers > $tmpfile
-      case $client in
-      "system-test-blueapi")
-      kcreg.sh create -x \
-      -s clientId=$client \
-      -s secret="secret" \
-      -s standardFlowEnabled=false \
-      -s serviceAccountsEnabled=true \
-      -s 'redirectUris=["/*"]' \
-      -f $tmpfile
-      ;;
-      "ixx-cli-blueapi")
-      protocolMappers='{
-        "protocolMappers": [
-          {
-            "name": "fedid",
-            "protocol": "openid-connect",
-            "protocolMapper": "oidc-hardcoded-claim-mapper",
-            "config": {
-              "introspection.token.claim": "true",
-              "claim.value": "alice",
-              "userinfo.token.claim": "true",
-              "id.token.claim": "true",
-              "access.token.claim": "true",
-              "claim.name": "fedid",
-              "jsonType.label": "String"
-            }
-          },
-          {
-            "name": "blueapi",
-            "protocol": "openid-connect",
-            "protocolMapper": "oidc-audience-mapper",
-            "config": {
-              "introspection.token.claim": "true",
-              "access.token.claim": "true",
-              "included.custom.audience": "blueapi"
-            }
-          },
-          {
-            "name": "ixx-blueapi",
-            "protocol": "openid-connect",
-            "protocolMapper": "oidc-audience-mapper",
-            "config": {
-              "introspection.token.claim": "true",
-              "access.token.claim": "true",
-              "included.custom.audience": "ixx-blueapi"
-            }
-          }
-        ]
-      }'
-      echo $protocolMappers > $tmpfile
-      kcreg.sh create -x \
-      -s clientId=$client \
-      -s standardFlowEnabled=false \
-      -s publicClient=true \
-      -s 'redirectUris=["/*"]' \
-      -s 'attributes={"frontchannel.logout.session.required":"true","oauth2.device.authorization.grant.enabled":"true","use.refresh.tokens":"true","backchannel.logout.session.required":"true"}' \
-      -f $tmpfile
-      ;;
-      "ixx-blueapi")
-      kcreg.sh create -x \
-      -s clientId=$client \
-      -s standardFlowEnabled=true \
-      -s publicClient=false \
-      -s secret="blueapi-secret" \
-      -s rootUrl="http://localhost:4180" \
-      -s adminUrl="http://localhost:4180" \
-      -s baseUrl="http://localhost:4180" \
-      -s 'redirectUris=["http://localhost:4180/*"]' \
-      -s 'webOrigins=["http://localhost:4180/*"]' \
-      -s 'attributes={"frontchannel.logout.session.required":"true","use.refresh.tokens":"true","standard.token.exchange.enabled": "true","standard.token.exchange.enableRefreshRequestedTokenType": "SAME_SESSION"}' \
-      -f $tmpfile
-      ;;
-      "tiled")
-      sed -i 's/blueapi/tiled/g' $tmpfile
-      kcreg.sh create -x \
-      -s clientId=$client \
-      -s standardFlowEnabled=true \
-      -s publicClient=false \
-      -s secret="tiled-secret" \
-      -s rootUrl="http://localhost:4181" \
-      -s adminUrl="http://localhost:4181" \
-      -s baseUrl="http://localhost:4181" \
-      -s 'redirectUris=["http://localhost:4181/*"]' \
-      -s 'webOrigins=["http://localhost:4181/*"]' \
-      -s 'attributes={"frontchannel.logout.session.required":"true","use.refresh.tokens":"true"}' \
-      -f $tmpfile
-      ;;
-      "tiled-cli")
-      sed -i 's/blueapi/tiled/g' $tmpfile
-      kcreg.sh create -x \
-      -s clientId=$client \
-      -s standardFlowEnabled=false \
-      -s publicClient=true \
-      -s 'redirectUris=["/*"]' \
-      -s 'attributes={"frontchannel.logout.session.required":"true","oauth2.device.authorization.grant.enabled":"true","use.refresh.tokens":"true","backchannel.logout.session.required":"true"}' \
-      -f $tmpfile
-      ;;
-      esac
-      rm $tmpfile
-  fi
-done
+    # Use sed to replace placeholders in the JSON template
+    sed "s/__AUDIENCE__/$aud/g; s/__CLAIM_VALUE__/alice/g" "$TEMPLATE" > "$tmpfile"
+
+    kcreg.sh create -x -s clientId="$client_id" -f "$tmpfile" "$@"
+    rm "$tmpfile"
+}
+
+# --- Create Clients ---
+
+# System Test
+create_client "system-test-blueapi" "blueapi" \
+    -s secret="secret" -s standardFlowEnabled=false -s serviceAccountsEnabled=true -s 'redirectUris=["/*"]'
+
+# ixx CLI
+create_client "ixx-cli-blueapi" "blueapi" \
+    -s standardFlowEnabled=false -s publicClient=true -s 'redirectUris=["/*"]' \
+    -s 'attributes={"oauth2.device.authorization.grant.enabled":"true","use.refresh.tokens":"true"}'
+
+# ixx BlueAPI
+create_client "ixx-blueapi" "blueapi" \
+    -s standardFlowEnabled=true -s secret="blueapi-secret" -s rootUrl="http://localhost:4180" \
+    -s 'redirectUris=["http://localhost:4180/*"]' \
+    -s 'attributes={"standard.token.exchange.enabled":"true"}'
+
+# Tiled
+create_client "tiled" "tiled" \
+    -s standardFlowEnabled=true -s secret="tiled-secret" -s rootUrl="http://localhost:4181" \
+    -s 'redirectUris=["http://localhost:4181/*"]'
+
+# Tiled CLI
+create_client "tiled-cli" "tiled" \
+    -s standardFlowEnabled=false -s publicClient=true -s 'redirectUris=["/*"]' \
+    -s 'attributes={"oauth2.device.authorization.grant.enabled":"true"}'

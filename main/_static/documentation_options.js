3332ce25c3cd3f51826b6d66c930c1eb284fa10a
abbiemery <abbiemery@users.noreply.github.com>
2026-02-26 17:03:40 +0000

    deploy: fb0258b4e45cf6be1847f209ac400a1d5835bfb4

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.12.1.dev6+ga438d99f2',
+    VERSION: '1.12.2.dev1+gfb0258b4e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

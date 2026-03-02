0744805aacb08a5407631486b28d10eb8b6dce09
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2026-03-02 11:14:39 +0000

    test: Make use of Sim to speedup test

--- tests/unit_tests/code_examples/device_module.py
+++ tests/unit_tests/code_examples/device_module.py
@@ -2,6 +2,6 @@ from dodal.common.beamlines.beamline_utils import device_factory
 from dodal.devices.bimorph_mirror import BimorphMirror
 
 
-@device_factory()
+@device_factory(mock=True)
 def oav() -> BimorphMirror:
     return BimorphMirror("BLXXI-BMRPH-01:", number_of_channels=8)

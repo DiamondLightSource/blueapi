98b9bbcfaba76f9e930464893cc6b2cb4770fe73
ZohebShaikh <ZohebShaikh@users.noreply.github.com>
2026-03-05 12:26:34 +0000

    deploy: 2e8842fbf2dada0185e172b7f30b67d43e42c3b8

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.12.2.dev6+g06bee206b',
+    VERSION: '1.12.2.dev7+g2e8842fbf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

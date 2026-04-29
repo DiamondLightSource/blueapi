db3dc7a76f8ca9e0af70e4321b049094e55d9a0f
alexj9837 <52531949+Alexj9837@users.noreply.github.com>
2026-04-29 16:03:43 +0000

    reduced the history of successful jobs, updated deletion script

--- helm/blueapi/files/scripts/pvc-deletion.sh
+++ helm/blueapi/files/scripts/pvc-deletion.sh
@@ -6,24 +6,21 @@ NOW=$(date +%s)
 #loop through all pvcs.
 for pvc in $BLUEAPI_PVCS; do
     #check if pvc has last-used annotation 
-    if kubectl get pvc $pvc -n  $RELEASE_NAMESPACE  -o=jsonpath='{.metadata.annotations.last-used}'
-    then
         #get last used annotation 
-        LAST_USED=$(kubectl get pvc $pvc -n  $RELEASE_NAMESPACE  -o=jsonpath='{.metadata.annotations.last-used}')
+        LAST_USED=$(kubectl get pvc "$pvc" -n  $RELEASE_NAMESPACE  -o=jsonpath='{.metadata.annotations.last-used}')
         #checking if its not null
         if [ -n "$LAST_USED"  ]; then
             #check if last_used is older than 3 months
             if [ $(($NOW - LAST_USED)) -gt 7884000 ]; then
                 #checking if the pvc is protected, if it is protected skip deletion
-                if  [ "$(kubectl get pvc $pvc -n  $RELEASE_NAMESPACE  -o=jsonpath='{.metadata.annotations.protected}')" = "true" ]; then
-                    echo "PVC $pvc is protected, skipping deletion"
+                if  [ "$(kubectl get pvc "$pvc" -n  $RELEASE_NAMESPACE  -o=jsonpath='{.metadata.annotations.protected}')" = "true" ]; then
+                    echo " PVC $pvc is protected, skipping deletion"
                     continue
                 fi
                 #PVC has not been used for more than three months, delete it
                 kubectl delete pvc "$pvc" -n  $RELEASE_NAMESPACE
             fi
-        fi
-    else
-         echo "PVC $pvc does not have last-used annotation, skipping deletion"
+        else
+            echo " $pvc has no last-used annotation"
     fi
 done

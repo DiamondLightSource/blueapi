33ae33f21b562123e35cbe73f9b62b6c0dbd7400
Peter Holloway <peter.holloway@diamond.ac.uk>
2026-04-29 10:58:17 +0100

    Move new endpoint to v2 api

--- src/blueapi/service/main.py
+++ src/blueapi/service/main.py
@@ -125,6 +125,7 @@ def lifespan(config: ApplicationConfig):
 open_router = APIRouter()
 secure_router = APIRouter(deprecated=True)
 secure_router_v1 = APIRouter(prefix="/api/v1")
+secure_router_v2 = APIRouter(prefix="/api/v2")
 
 
 def get_app(config: ApplicationConfig):
@@ -146,6 +147,7 @@ def get_app(config: ApplicationConfig):
         }
     app.include_router(open_router)
     app.include_router(secure_router_v1, dependencies=dependencies)
+    app.include_router(secure_router_v2, dependencies=dependencies)
     app.include_router(secure_router, dependencies=dependencies)
     app.add_exception_handler(KeyError, on_key_error_404)
     app.add_exception_handler(jwt.PyJWTError, on_token_error_401)
@@ -581,7 +583,7 @@ def logout(runner: Annotated[WorkerDispatcher, Depends(_runner)]) -> Response:
     )
 
 
-@secure_router.websocket("/run_plan")
+@secure_router_v2.websocket("/run_plan")
 async def run_plan(
     ws: WebSocket,
     user: Fedid,

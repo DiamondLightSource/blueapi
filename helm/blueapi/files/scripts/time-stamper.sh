1f47921e5dc014b6b330899674db2c3d36abb433
Alex J <52531949+Alexj9837@users.noreply.github.com>
2026-05-01 10:14:43 +0100

    Annotate blueapi PVCs with last-used timestamp

--- helm/blueapi/files/scripts/time-stamper.sh
+++ helm/blueapi/files/scripts/time-stamper.sh
@@ -2,8 +2,9 @@
 # Get all PVCs currently mounted by running pods
 MOUNTED_PVCS=$(kubectl get pods -n  $RELEASE_NAMESPACE  \
   -o=jsonpath='{.items[*].spec.volumes[*].persistentVolumeClaim.claimName}' | tr ' ' '\n' | sort -u)
-NOW=$(date +%s)
+BLUEAPI_PVCS=$( echo $MOUNTED_PVCS | tr ' ' '\n' | grep blueapi-scratch)
 #loop through all the pvcs annotating ones thare are mounted
-for pvc in $MOUNTED_PVCS; do
+NOW=$(date +%s)
+for pvc in $BLUEAPI_PVCS; do
     kubectl annotate --overwrite pvc "$pvc" -n  $RELEASE_NAMESPACE  last-used="$NOW"
 done

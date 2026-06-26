65a3b9d8f8f0f24834419271e8f87d1f494f8e84
Abigail Emery <abigail.emery@diamond.ac.uk>
2026-06-26 10:08:01 +0000

    Add dodal plans to task worker unit tests

--- tests/unit_tests/worker/test_task_worker.py
+++ tests/unit_tests/worker/test_task_worker.py
@@ -20,7 +20,7 @@ from observability_utils.tracing import (
 )
 from ophyd_async.core import AsyncStatus
 
-from blueapi.config import DeviceManagerSource, EnvironmentConfig
+from blueapi.config import DeviceManagerSource, EnvironmentConfig, PlanSource
 from blueapi.core import BlueskyContext, EventStream
 from blueapi.core.bluesky_types import DataEvent
 from blueapi.service.model import PlanModel
@@ -109,7 +109,12 @@ def second_fake_device() -> FakeDevice:
 @pytest.fixture
 def context(fake_device: FakeDevice, second_fake_device: FakeDevice) -> BlueskyContext:
     ctx = BlueskyContext()
-    ctx_config = EnvironmentConfig()
+    ctx_config = EnvironmentConfig(
+        sources=[
+            PlanSource(module="dodal.plans"),
+            PlanSource(module="dodal.plan_stubs.wrapped"),
+        ]
+    )
     ctx_config.sources.append(DeviceManagerSource(module="devices"))
     ctx.register_plan(failing_plan)
     ctx.register_device(fake_device)
@@ -121,7 +126,12 @@ def context(fake_device: FakeDevice, second_fake_device: FakeDevice) -> BlueskyC
 @pytest.fixture
 def context_without_devices() -> BlueskyContext:
     ctx = BlueskyContext()
-    ctx_config = EnvironmentConfig()
+    ctx_config = EnvironmentConfig(
+        sources=[
+            PlanSource(module="dodal.plans"),
+            PlanSource(module="dodal.plan_stubs.wrapped"),
+        ]
+    )
     ctx_config.sources.append(DeviceManagerSource(module="devices"))
     ctx.with_config(ctx_config)
     return ctx

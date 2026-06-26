d0ebd45bd67a08fedcebe5acb74d0a8fdcfb6f27
Abigail Emery <abigail.emery@diamond.ac.uk>
2026-06-26 10:05:53 +0000

    Remove dodal plans from default config

--- src/blueapi/config.py
+++ src/blueapi/config.py
@@ -140,10 +140,7 @@ class EnvironmentConfig(BlueapiBaseModel):
             PlanSource | DeviceManagerSource,
             Field(discriminator="kind"),
         ]
-    ] = [
-        PlanSource(module="dodal.plans"),
-        PlanSource(module="dodal.plan_stubs.wrapped"),
-    ]
+    ] = Field(default=[])
     events: WorkerEventConfig = Field(default_factory=WorkerEventConfig)
     metadata: MetadataConfig | None = Field(default=None)
 
